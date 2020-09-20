ed74cfd5ce61d889c3c11f2a7d3027e71b57429f
Danielle <niellefranca@gmail.com>
2020-09-20 11:16:48 -0300

    primeiro commit projeto inicial

---
+++ src/main/java/br/com/daniellefranca/cursoumc/CursoumcApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniellefranca.cursoumc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoumcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoumcApplication.class, args);
+	}
+
+}
