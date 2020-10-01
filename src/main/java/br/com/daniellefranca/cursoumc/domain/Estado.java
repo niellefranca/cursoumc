02a6dc8e712dc7b43bc9155026659aab5c66a4ac
niellefranca <niellefranca@gmail.com>
2020-10-01 17:17:39 -0300

    estado com erro no commmit

--- src/main/java/br/com/daniellefranca/cursoumc/domain/Estado.java
+++ src/main/java/br/com/daniellefranca/cursoumc/domain/Estado.java
@@ -8,11 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Estado implements Serializable {
